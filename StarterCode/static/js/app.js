e422df9d959341b3b412e63ad896d84daaf5e93c
Alex Martinez <alexmartinez1745@gmail.com>
2021-04-05 19:49:15 -0400

    start of bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,15 +8,17 @@ updatePlots = (data) => {
 
 handleChange = (data) => {
     let id = d3.event.target.value;
-    updatePlots(data, id)
+    updatePlots(data)
 }
 
 // Let d3 json load first when you open up the index html
+// Use of D3 library to read in the samples data
 d3.json("samples.json").then(data => {
     console.log(data);
     // Set up initial stuff
     // Filter our samples to element 940, then build plots
         //filter data.samples and only return the ones where id is equal to 940
+
     // for bubble:
     // y vals and size are the sample_values
     // x vals are otu_ids
@@ -25,17 +27,24 @@ d3.json("samples.json").then(data => {
     // for hbar:
     // x vals are sample_values
     // y vals are otu_ids
+    let id = d3.event.target.value;
+    let sample = data.samples.filter(sample => sample.id === id)
+    let otuid = sample.otu_ids.slice(0,10)
+    let sample_values = sample.sample_values.slice(0,10)
+
+    trace1 = {
+        type: "bar",
+        orientation: "h",
+        x: sample_values,
+        y: otuid,
+    }
 
     // for demographic info:
     // Filter metdata for id
     // set internal html of demographic info card
 
-    //for dropdown list:
-    // create an option element for all names
-    // be sure to set value of the option to ypur filter value
-
+    // Populating select options from data in the html
     let mySelect = d3.select("#selDataset");
-    // what we know how to do for right now day 2
     data.names.forEach(element => {
         mySelect.append("option").attr("value", element).text(element);
     })
